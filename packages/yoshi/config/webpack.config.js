80a2d851d695d898bcabeee34378c3efc547b1b8
liat ashkenazi <liata@wix.com>
2019-06-20 14:53:12 +0300

    yoshi| disable rtl.css when using postcss-rtl

--- packages/yoshi/config/webpack.config.js
+++ packages/yoshi/config/webpack.config.js
@@ -726,7 +726,7 @@ function createClientWebpackConfig({
             // https://github.com/wix-incubator/tpa-style-webpack-plugin
             ...(project.enhancedTpaStyle ? [new TpaStyleWebpackPlugin()] : []),
             // https://github.com/wix/rtlcss-webpack-plugin
-            ...(!project.experimentalBuildHtml
+            ...(!project.experimentalBuildHtml && !project.experimentalRtlCss
               ? [
                   new RtlCssPlugin(
                     isDebug ? '[name].rtl.css' : '[name].rtl.min.css',
